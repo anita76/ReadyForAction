b6a456b069220653eb51334993757440a961498a
cindy-yhtu <yu-hsin.tu@alumni.ubc.ca>
2019-03-09 18:48:40 -0800

    working on checklist

---
+++ app/src/main/java/com/cmdf2019/readyforaction/Checklist.java
@@ -0,0 +1,15 @@
+package com.cmdf2019.readyforaction;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Checklist {
+    public Map<String, String> map;
+
+    public Checklist() {
+        map = new TreeMap<>();
+    }
+    
+
+
+}
