b3e2ff06f5e678aac3614909554586f9c195ccaf
Jude Shamsi <judeshamsi@dhcp-128-189-84-104.ubcsecure.wireless.ubc.ca>
2019-03-09 21:41:27 -0800

    page 4 for

---
+++ app/src/main/java/com/cmdf2019/readyforaction/ChecklistEntry1.java
@@ -0,0 +1,13 @@
+package com.cmdf2019.readyforaction;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChecklistEntry1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_checklist_entry1);
+    }
+}
