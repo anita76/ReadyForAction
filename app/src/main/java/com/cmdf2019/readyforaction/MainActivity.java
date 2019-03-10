d5d7260eeffc134a811bce11d0c81784318000b0
cindy-yhtu <yu-hsin.tu@alumni.ubc.ca>
2019-03-09 16:28:26 -0800

    first to second page with button

--- app/src/main/java/com/cmdf2019/readyforaction/MainActivity.java
+++ app/src/main/java/com/cmdf2019/readyforaction/MainActivity.java
@@ -1,13 +1,31 @@
 package com.cmdf2019.readyforaction;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        nextPage = (Button) findViewById(R.id.next_button);
+        nextPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToActionPage();
+            }
+        });
+
+    }
+
+    private void goToActionPage() {
+        Intent intent = new Intent(this, action.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/cmdf2019/readyforaction/action.java
+++ app/src/main/java/com/cmdf2019/readyforaction/action.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class action extends AppCompatActivity {
+public class action extends AppCompatActivity implements View.OnClickListener {
     Button helpButton;
 
     @Override
@@ -13,17 +13,26 @@ public class action extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_action);
 
-        helpButton = (Button) findViewById(R.id.help_button);
-        helpButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Code here executes on main thread after user presses button
-            }
-        });
+//        helpButton = (Button) findViewById(R.id.help_button);
+//        helpButton.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Code here executes on main thread after user presses button
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View view)
+    {
+        switch (view.getId()) {
+            case R.id.help_button:
+                sendText();
+        }
     }
 
     // send a text message to contact on button click
     // text include n=info like what role and step user is on
-    public void sendText(View view)
+    public void sendText()
     {
         // Do something in response to button click
     }
