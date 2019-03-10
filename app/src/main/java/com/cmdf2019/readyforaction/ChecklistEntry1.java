df74222853c7ce752095993c417fa968c82b7830
Jude Shamsi <judeshamsi@dhcp-128-189-84-104.ubcsecure.wireless.ubc.ca>
2019-03-09 23:35:46 -0800

    merged cindy

--- app/src/main/java/com/cmdf2019/readyforaction/ChecklistEntry1.java
+++ app/src/main/java/com/cmdf2019/readyforaction/ChecklistEntry1.java
@@ -2,17 +2,49 @@ package com.cmdf2019.readyforaction;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
-public class ChecklistEntry1 extends AppCompatActivity {
+import java.util.ArrayList;
 
-//    private EditText itemET;
-//    private Button btn;
-//    private
+public class ChecklistEntry1 extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText itemET;
+    private Button btn;
+    private ListView itemsList;
+
+
+    private ArrayList<String> items;
+    private ArrayAdapter<String> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checklist_entry1);
+
+        itemET = findViewById(R.id.item_edit_text);
+        btn = findViewById(R.id.add_btn);
+        itemsList = findViewById(R.id.items_list);
+
+        btn.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.add_btn:
+                String itemEnetered= itemET.getText().toString();
+                adapter.add(itemEnetered);
+                itemET
+
+                break;
+        }
+
     }
 }
