a5bfee2f4c7399c775b745778be534396d44a0aa
cindy-yhtu <yu-hsin.tu@alumni.ubc.ca>
2019-03-09 22:33:59 -0800

    setting up to merge anita

--- app/src/main/java/com/cmdf2019/readyforaction/MainActivity.java
+++ app/src/main/java/com/cmdf2019/readyforaction/MainActivity.java
@@ -4,27 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button nextPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        nextPage = (Button) findViewById(R.id.next_button);
-        nextPage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToActionPage();
-            }
-        });
     }
 
-    private void goToActionPage() {
-        Intent intent = new Intent(this, action.class);
-        startActivity(intent);
+    public void goToHomePage(View view){
+        Intent startHomePageActivity = new Intent(this, HomePageActivity.class);
+        startActivity(startHomePageActivity);
     }
 }
